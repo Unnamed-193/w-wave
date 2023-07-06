58c2609fd7cc35d3694d3de97bb337c254434476
Unnamed-193 <133913095+Unnamed-193@users.noreply.github.com>
2023-07-06 23:28:30 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// Якори
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for(let anchor of anchors){
+  anchor.addEventListener('click', function(event){
+    event.preventDefault();
+   
+    const blockID = anchor.getAttribute('href').substring(1);
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
+
