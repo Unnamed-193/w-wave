886a3c6116577aa0dd6798358c4990be7bf760f1
Kirill <k187m18733@gmail.com>
2025-01-17 17:08:57 +0300

    fix

--- js/index.js
+++ js/index.js
@@ -14,3 +14,18 @@ for(let anchor of anchors){
   })
 }
 
+// const header = document.querySelector('.header');
+// const hero = document.querySelector('.hero').clientHeight;
+
+// document.addEventListener('scroll', () => {
+//     const scroll = window.scrollY;
+
+//   if(scroll >= hero){
+//     header.classList.add('fixed');
+    
+ 
+//   } else{
+//     header.classList.remove('fixed');
+   
+//   }
+// })
